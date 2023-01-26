c11f142410c5eaaec168f15b87cf6d2054b1225d
bahaa <malkebahaa450>
2023-01-26 05:21:29 +0200

    any10

--- 5_getting_the_smartest_and_least_smart_customer.js
+++ 5_getting_the_smartest_and_least_smart_customer.js
@@ -15,12 +15,14 @@ import { arr_of_JAVAscript_objects_from_db, m_read_customer_data_from_databas } 
 //#endregion
 //#region variables
 let arr_of_customers_IQ = [];
+let the_smartest_customer = {};
+let the_least_smartest_customer = {};
 
 //#endregion
 //#region invoked function
 
-get_the_smartest_customer_and_its_detailes();
-
+await get_the_smartest_customer_and_its_detailes();
+console.log(the_smartest_customer);
 //#endregion
 
 //#region functions
@@ -29,7 +31,18 @@ async function get_the_smartest_customer_and_its_detailes() {
   s_getting_arr_of_customers_IQ();
   //   console.log(arr_of_customers_IQ);
   let max_IQ = Math.max(...arr_of_customers_IQ);
-  console.log(max_IQ);
+  //   console.log(max_IQ);
+  for (let i = 0; i < arr_of_JAVAscript_objects_from_db.length; i++) {
+    if (arr_of_JAVAscript_objects_from_db[i].IQ == max_IQ) {
+      the_smartest_customer = {
+        the_smartest_customer_IQ: arr_of_JAVAscript_objects_from_db[i].IQ,
+        the_smartest_customer_Name: arr_of_JAVAscript_objects_from_db[i].name,
+        the_smartest_customer_Job: arr_of_JAVAscript_objects_from_db[i].job,
+        the_smartest_customer__age: arr_of_JAVAscript_objects_from_db[i].age,
+      };
+    }
+  }
+  return the_smartest_customer;
 }
 
 function s_getting_arr_of_customers_IQ() {
